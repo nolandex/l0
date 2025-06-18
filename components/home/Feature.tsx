5999f9fe34bf099e0df52e6874298e5aa9dcdd2c
nolandex <stemapal1234@gmail.com>
2025-06-18 12:52:57 +0700

    Update Feature.tsx

--- components/home/Feature.tsx
+++ components/home/Feature.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { ALL_FEATURES } from "@/config/feature";
 import React from "react";
 import { RoughNotation } from "react-rough-notation";
@@ -12,43 +14,39 @@ const Feature = ({
   langName: string;
 }) => {
   const FEATURES = ALL_FEATURES[`FEATURES_${langName.toUpperCase()}`];
+
   return (
-    <section
-      id={id}
-      className="flex flex-col justify-center lg:max-w-7xl md:max-w-5xl w-[95%] mx-auto md:gap-14 pt-16"
-    >
-      <h2 className="text-center text-white">
-        <RoughNotation type="highlight" show={true} color="#2563EB">
-          {locale.title}
-        </RoughNotation>
-      </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        {FEATURES?.map((feature, index) => (
-          <div
-            key={feature.title}
-            className={`
-              flex flex-col items-center text-center px-8 py-6 border-b
-              ${index === 0 ? "md:border-r" : ""}
-              ${index === 1 ? "lg:border-r" : ""}
-              ${index === 2 ? "md:border-r lg:border-r-0" : ""}
-              ${index === 3 ? "lg:border-b-0 lg:border-r" : ""}
-              ${index === 4 ? "md:border-b-0 md:border-r" : ""}
-              ${index === 5 ? "border-b-0 border-r-0" : ""}
-            `}
-          >
-            <div className="p-4 w-16 h-16 dark:text-white rounded-full flex items-center justify-center">
-              {feature.icon && typeof feature.icon === "string" ? (
-                <span className="text-2xl">{feature.icon}</span>
-              ) : (
-                React.createElement(feature.icon, { className: "text-2xl" })
-              )}
+    <section id={id}>
+      <div className="container space-y-6 rounded-md bg-secondary py-14 lg:py-24">
+        <div className="mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center">
+          <h2 className="font-heading text-4xl md:text-6xl text-white">
+            <RoughNotation type="highlight" show={true} color="#2563EB">
+              {locale.title}
+            </RoughNotation>
+          </h2>
+        </div>
+        <div className="mx-auto grid justify-center gap-4 text-center sm:grid-cols-2 md:max-w-[64rem] md:grid-cols-3">
+          {FEATURES?.map((feature) => (
+            <div
+              key={feature.title}
+              className="flex h-[200px] flex-col justify-between rounded-md bg-background/70 p-6 shadow-md border dark:border-muted"
+            >
+              <div className="flex items-center justify-center w-12 h-12 rounded-full bg-primary/10 text-primary mx-auto mb-4">
+                {feature.icon && typeof feature.icon === "string" ? (
+                  <span className="text-2xl">{feature.icon}</span>
+                ) : (
+                  React.createElement(feature.icon, {
+                    className: "text-2xl",
+                  })
+                )}
+              </div>
+              <h3 className="text-lg font-semibold text-white mb-1">
+                {feature.title}
+              </h3>
+              <p className="text-sm text-muted-foreground">{feature.content}</p>
             </div>
-            <h2 className={"text-xl font-semibold mb-2"}>{feature.title}</h2>
-            <p className="text-slate-700 dark:text-slate-400">
-              {feature.content}
-            </p>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </section>
   );
