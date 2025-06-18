48878f04e2b8594ec0e873d0c6ec294c20bae2d2
nolandex <stemapal1234@gmail.com>
2025-06-18 13:57:00 +0700

    Update HomeIndex.tsx

--- components/home/HomeIndex.tsx
+++ components/home/HomeIndex.tsx
@@ -3,8 +3,6 @@ import FAQ from "@/components/home/FAQ";
 import Feature from "@/components/home/Feature";
 import Hero from "@/components/home/Hero";
 import Pricing from "@/components/home/Pricing";
-import ScrollingLogos from "@/components/home/ScrollingLogos";
-import SocialProof from "@/components/home/SocialProof";
 import Testimonials from "@/components/home/Testimonials";
 import { defaultLocale, getDictionary } from "@/lib/i18n";
 
@@ -16,9 +14,6 @@ export default async function HomeIndex({ lang }: { lang: string }) {
     <>
       {/* Hero Section */}
       <Hero locale={dict.Hero} langName={langName} CTALocale={dict.CTAButton} />
-      <SocialProof locale={dict.SocialProof} />
-      {/* display technology stack, partners, project honors, etc. */}
-      <ScrollingLogos />
 
       {/* Showcase */}
       {/* <Showcase id="Showcase" locale={dict.Showcase} /> */}
