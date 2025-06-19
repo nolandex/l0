79195ab27f782897e2966c8ba68852009a38ddfd
nolandex <stemapal1234@gmail.com>
2025-06-19 12:33:16 +0700

    Update Hero.tsx

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -34,7 +34,7 @@ const Hero = ({
         lang={langName}
         className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-16 pt-24 md:pt-32 text-center"
       >
-        <h1>
+        <h1 className="text-5xl sm:text-6xl md:text-7xl font-extrabold tracking-tight text-slate-900 dark:text-white">
           {locale.title1} <LineText>{locale.title2}</LineText> {locale.title3}
         </h1>
         <p className="mx-auto mt-6 max-w-2xl text-2xl tracking-tight text-slate-700 dark:text-slate-400">
