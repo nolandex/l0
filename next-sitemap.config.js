24e0e5f0e3e119c726668aceec25465d6ad6ff0c
nolandex <stemapal1234@gmail.com>
2025-06-20 10:19:08 +0700

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
-
 module.exports = {
-  siteUrl:
-    process.env.NEXT_PUBLIC_SITE_URL || "https://landingpage.weijunext.com",
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://bisnovo.my.id", // GANTI DARI landingpage.weijunext.com
   generateRobotsTxt: true,
   sitemapSize: 7000,
 };
